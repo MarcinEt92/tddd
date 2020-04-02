e4a898da1a8f0310976cde127d379dd9801c0a67
Korman <marcinx.korman@intel.com>
2020-04-02 14:32:56 +0200

    commit before creating a database

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -11,6 +12,12 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # open a website
@@ -30,9 +37,17 @@ class NewVisitorTest(unittest.TestCase):
 
         # type Enter and then site content is updated and displayed
         input_box.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = self.browser.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "no such element in table")
+
+        #  time.sleep(10)
+
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Use peacock feather to make a bait')
+        input_box.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feather to make a bait")
 
         self.fail('end of test')
 
